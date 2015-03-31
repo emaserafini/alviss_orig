340094ad38ed0d025b00be0c5bec2c0694084f04
Emanuele Serafini <ema.serafini@gmail.com>
2015-03-31 11:37:44 +0200

    Datapoint::Temperature model spec

--- app/models/datapoint/temperature.rb
+++ app/models/datapoint/temperature.rb
@@ -1,3 +1,7 @@
 class Datapoint::Temperature < ActiveRecord::Base
   belongs_to :stream
+  validates :stream, presence: true
+  validates :value, numericality: true
+
+  scope :of_stream, -> (id) { where(stream_id: id) }
 end
