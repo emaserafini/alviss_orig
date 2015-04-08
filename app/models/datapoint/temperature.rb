87b4eb20490c09dc51bfc570be2d73674f021cdf
Emanuele Serafini <ema.serafini@gmail.com>
2015-04-08 15:53:32 +0200

    add scope in Datapoint::Temperature

--- app/models/datapoint/temperature.rb
+++ app/models/datapoint/temperature.rb
@@ -3,5 +3,10 @@ class Datapoint::Temperature < ActiveRecord::Base
   validates :stream, presence: true
   validates :value, numericality: true
 
-  scope :of_stream, -> (id) { where(stream_id: id) }
+  scope :of_stream, -> (stream_id) { where(stream_id: stream_id) }
+  scope :latest, -> (num) { order('created_at DESC').limit(num) }
+
+  def self.latest_of_stream stream_id, num
+    of_stream(stream_id).latest(num)
+  end
 end
